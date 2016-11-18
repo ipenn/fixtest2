12ae8fe06f089e43fdd1383d6196473403bd6eaf
Som Ghosh <som.ghosh@gmail.com>
2016-11-18 15:34:57 +0000

    with comments

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import quickfix as fix
-import quickfix44 as fixMsg
+import quickfix44 as fix44
+
 import datetime
 import random
 
@@ -12,6 +13,36 @@ class Application (fix.Application):
     def onLogon(self, sessionID):
         self.sessionID = sessionID
         print("onLogon - sessionID: " + sessionID.toString())
+
+        mdr = fix.Message()
+        mdr.getHeader().setField(fix.BeginString(fix.BeginString_FIX44))
+        mdr.getHeader().setField(fix.MsgType(fix.MsgType_MarketDataRequest))
+
+        mdr.setField(fix.MDReqID('17499348912123123'))      # a random string
+        mdr.setField(fix.SubscriptionRequestType(fix.SubscriptionRequestType_SNAPSHOT_PLUS_UPDATES))        # what stater required
+        mdr.setField(fix.MarketDepth(1))        # what stater required
+
+        mdr.setField(fix.AggregatedBook(True))
+
+        mdr.setField(fix.NoMDEntryTypes(1))     # what stater required
+        mdr.setField(fix.MDUpdateType(fix.MDUpdateType_INCREMENTAL_REFRESH))        # what stater required
+
+        group = fix44.MarketDataRequest().NoMDEntryTypes()
+        group.setField(fix.MDEntryType(fix.MDEntryType_BID))
+        mdr.addGroup(group)
+        group.setField(fix.MDEntryType(fix.MDEntryType_OFFER))
+        mdr.addGroup(group)
+
+        mdr.setField(fix.NoRelatedSym(1))
+
+        symbol = fix44.MarketDataRequest().NoRelatedSym()
+
+        symbol.setField(fix.Symbol('GBP/USD'))
+        mdr.addGroup(symbol)
+
+        fix.Session.sendToTarget(mdr, sessionID)
+
+
 
     def onLogout(self, sessionID):
         self.sessionID = sessionID
@@ -45,3 +76,6 @@ class Application (fix.Application):
         self.sessionID = sessionID
         self.message = message
         print("fromApp - message: " + message.toString() + " sessionID:" + sessionID.toString())
+
+
+
